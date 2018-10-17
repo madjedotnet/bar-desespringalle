b79c6385c7ae0abc18d8823a5755bb62d001628f
Matthieu Bar-Desespringalle <madje@MacBook-Pro-de-Matthieu.local>
2018-10-17 14:05:38 +0200

    livraison des éléments du tuto

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller {
 
     /**
+     * @Route("/bonjour/{prenom}/age/{age}", name="hello")
+     * @Route("/salut", name="hello_base")
+     * @Route("/bonjour/{prenom}", name="hello_prenom")
+     * 
+     * @return void
+     */
+    public function hello($prenom = "anonyme", $age = 0) {
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom,
+            'age' => $age
+        ]);
+    }
+
+    /**
      * @Route("/", name="homepage")
      *
      */
