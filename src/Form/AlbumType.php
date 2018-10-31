a903cf5990aa6c21f74c5b2d28c0f7aca25994dc
madjedotnet <matthieu@bar-desespringalle.fr>
2018-10-31 23:31:16 +0100

    factorisation du code pour le formulaire de création d'album

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -6,16 +6,34 @@ use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AlbumType extends AbstractType
 {
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     * 
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder) {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('slug')
-            ->add('title')
-            ->add('content')
+            // ->add('slug')
+            ->add('title', TextType::class, $this->getConfiguration("Titre", "Taper un titre pour ce nouvel album !"))
             ->add('albumDate')
+            ->add('content', TextareaType::class, $this->getConfiguration("Description", "Taper une description pour ce nouvel album !"))
             ->add('creationDate')
             ->add('creationUser')
         ;
