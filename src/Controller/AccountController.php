e9ed9adcff2f85edd3e421c31fcd25a44804fd08
madje <matthieu@bar-desespringalle.fr>
2018-11-14 14:00:01 +0100

    création du fomulaire d'enregistrement de nouvel utilisateur, ainsi que l'enregistrement en base de données

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -15,8 +21,16 @@ class AccountController extends AbstractController
      * 
      * @return Response
      */
-    public function login() {
-        return $this->render('account/login.html.twig');
+    public function login(AuthenticationUtils $utils) {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        dump($error);
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
     }
 
     /**
@@ -28,5 +42,38 @@ class AccountController extends AbstractController
      */
     public function logout() {
 
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'inscription
+     * 
+     * @Route("/register", name="account_register")
+     *
+     * @return Response
+     */
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder) {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success', "Votre compte a bien été créé !"
+            );
+
+            return $this->redirectToRoute('account_login');
+        }
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -4,34 +4,15 @@ namespace App\Form;
 
 use App\Entity\Album;
 use App\Form\PictureType;
-use Symfony\Component\Form\AbstractType;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
-class AlbumType extends AbstractType
-{
-    /**
-     * Permet d'avoir la configuration de base d'un champ
-     * 
-     * @param string $label
-     * @param string $placeholder
-     * @param array $options
-     * @return array
-     */
-    private function getConfiguration($label, $placeholder, $options = []) {
-        return array_merge([
-            'label' => $label,
-            'attr' => [
-                'placeholder' => $placeholder
-            ]
-        ], $options);
-    }
-
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class AlbumType extends ApplicationType {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add(
                 'title', 
