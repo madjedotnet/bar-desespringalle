2e7159ffef4322bc6852ad948ea8474742caaf8b
madje <matthieu@bar-desespringalle.fr>
2018-12-17 13:55:46 +0100

    Attention erreur lors de la modification d'album - suppression de photo

--- src/Controller/AdminAlbumController.php
+++ src/Controller/AdminAlbumController.php
@@ -15,8 +15,7 @@ class AdminAlbumController extends AbstractController
     /**
      * @Route("/admin/albums", name="admin_albums_index")
      */
-    public function index(AlbumRepository $repo)
-    {
+    public function index(AlbumRepository $repo) {
         return $this->render('admin/album/index.html.twig', [
             'albums' => $repo->findAll()
         ]);
